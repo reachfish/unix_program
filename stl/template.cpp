e89c2efb9afe291431480c49d6729e1239cae26b
yujun <yu.jun.reach@gmail.com>
2017-11-27 20:57:11 +0800

    template内部还可以有template

--- stl/template.cpp
+++ stl/template.cpp
@@ -4,34 +4,56 @@
 // template: 一般化和特殊化设计
 //
 template<class I, class O>
-struct template_foo{
-	template_foo(){
+struct template_foo_1{
+	template_foo_1(){
 		std::cout<<"<class I, class O>"<<std::endl;
 	}
 };
 
 template<class T>
-struct template_foo<T*, T*>{
-	template_foo(){
+struct template_foo_1<T*, T*>{
+	template_foo_1(){
 		std::cout<<"<T*, T*>"<<std::endl;
 	}
 };
 
 template<class T>
-struct template_foo<const T*, T*>{
-	template_foo(){
+struct template_foo_1<const T*, T*>{
+	template_foo_1(){
 		std::cout<<"<const T*, T*>"<<std::endl;
 	}
 };
 
 void test1(){
-	template_foo<int, int> foo1;
-	template_foo<int*, int*> foo2;
-	template_foo<const int*, int*> foo3;
+	template_foo_1<int, int> foo1;
+	template_foo_1<int*, int*> foo2;
+	template_foo_1<const int*, int*> foo3;
 }
 
 /*********************************************/
+// template: template 内部还可以有template
+
+template<class T>
+struct template_foo_2{
+
+	typedef T value_type;
+	typedef value_type* iterator;
+
+	template<class I>
+	void print(iterator position, I first, I end){
+		std::cout<<"template_foo_2::print:"<<first<<","<<end<<std::endl;
+	}
+};
+
+void test2(){
+	template_foo_2<int> foo;
+	template_foo_2<int>::iterator it;
+
+	foo.print(it, 10, 100);
+}
+/*********************************************/
+
 int main(){
-	test1();
+	test2();
 	return 0;
 }
