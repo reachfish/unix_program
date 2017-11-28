9c9fe362da59e624a2acce0d4e11909a6e0f0c20
yujun <yu.jun.reach@gmail.com>
2017-11-28 16:51:37 +0800

    template<> macro

--- stl/template.cpp
+++ stl/template.cpp
@@ -52,8 +52,69 @@ void test2(){
 	foo.print(it, 10, 100);
 }
 /*********************************************/
+// template: __STL_NULL_TMPL_ARGS<>
+
+#ifndef __STL_NULL_TMPL_ARGS 
+#define __STL_NULL_TMPL_ARGS <>
+#endif
+
+template<class T>
+struct template_foo_3{
+	//friend bool operator==__STL_NULL_TMPL_ARGS (const template_foo_3&, const template_foo_3&);
+	//friend bool operator<__STL_NULL_TMPL_ARGS (const template_foo_3&, const template_foo_3&);
+
+	//展开后就变成了
+	//friend bool operator==<> (const template_foo_3&, const template_foo_3&);
+	//friend bool operator<<> (const template_foo_3&, const template_foo_3&);
+	
+	//写成这样是可以的
+	//friend bool operator==<T> (const template_foo_3<T>&, const template_foo_3<T>&);
+	//friend bool operator< <T> (const template_foo_3<T>&, const template_foo_3<T>&);
+	
+	//写成这样也是可以的
+	//friend bool operator==<T> (const template_foo_3&, const template_foo_3&);
+	//friend bool operator< <T> (const template_foo_3&, const template_foo_3&);
+	
+	//写成这样也是可以的
+	//friend bool operator==<> (const template_foo_3&, const template_foo_3&);
+	//friend bool operator< <> (const template_foo_3&, const template_foo_3&);
+	
+	//写成这样是不可以的
+	//friend bool operator== (const template_foo_3&, const template_foo_3&);
+	//friend bool operator< (const template_foo_3&, const template_foo_3&);
+};
+/*********************************************/
+// template: __STL_TEMPLATE_NULL 展开为  template<>
+
+#ifndef __STL_TEMPLATE_NULL 
+#define __STL_TEMPLATE_NULL template<>
+#endif
+
+template<class T>
+struct template_foo_4{
+	void operator()() {
+		std::cout<<"template_foo_4:"<<std::endl;
+	}
+};
+
+// explicit specialization
+__STL_TEMPLATE_NULL
+struct template_foo_4<char>{
+	void operator()() {
+		std::cout<<"template_foo_4: template<char>"<<std::endl;
+	}
+};
+
+void test4(){
+	template_foo_4<int> t1;
+	template_foo_4<char> t2;
+
+	t1();
+	t2();
+};
+/*********************************************/
 
 int main(){
-	test2();
+	test4();
 	return 0;
 }
